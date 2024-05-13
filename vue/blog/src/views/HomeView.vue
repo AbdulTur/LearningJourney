90efe98a33a941ff921f63521df3d1d3dbd93d52
Abdul <aturonov@mun.ca>
2024-05-13 16:45:07 -0230

    finishing up

---
+++ vue/blog/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    Hello World
+  </div>
+  <p>My name is {{ name }} and my age is {{ age }}</p>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomeView',
+  setup() {
+    console.log('setup')
+    let name = 'abdul'
+    let age = 22
+    return {name, age}
+  }
+
+}
+
+</script>
