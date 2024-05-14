b9d8e4cf55be57b67cb0825f04d56d7396260537
Abdul <aturonov@mun.ca>
2024-05-14 14:02:54 -0230

    finished

---
+++ vue/blog/src/components/PostList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="post-list">
+      <div v-for="post in posts" :key="post.id">
+        <!-- <h3>{{ post.title }}</h3> -->
+        <SinglePost :post="post" />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  // component imports
+  import { onMounted } from 'vue';
+  import SinglePost from './SinglePost.vue'
+  
+  export default {
+    props: ['posts'],
+    components: { SinglePost },
+    setup(props) {
+      onMounted(() => {console.log('component mounted')})
+    }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
